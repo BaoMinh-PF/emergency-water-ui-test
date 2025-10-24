8ceb3381a2e08f4451bf372203082ef42831d124
ming <ming@VNMPCMING02>
2025-10-24 20:59:55 +0700

    Test

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/emergency-water-ui-test" : "",
-  assetPrefix: isProd ? "/emergency-water-ui-test" : "",
+  basePath: isDev ? "" : "/emergency-water-ui-test",
+  assetPrefix: isDev ? "" : "/emergency-water-ui-test",
 };
 
 export default nextConfig;
