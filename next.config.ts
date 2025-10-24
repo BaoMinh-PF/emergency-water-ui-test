67dba63c9882ef007f1da5b23a41becae87e785f
ming <ming@VNMPCMING02>
2025-10-24 20:52:16 +0700

    Update

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   basePath: isProd ? "/emergency-water-ui-test" : "",
+  assetPrefix: isProd ? "/emergency-water-ui-test" : "",
 };
 
 export default nextConfig;
